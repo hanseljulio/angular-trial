07dee81c5071a6601e1ec51daddb0586a5dbf09c
hanseljulio <hanseljulio@yahoo.com>
2020-09-04 12:27:03 -0700

    2 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,14 +5,17 @@ export class CartService {
   items = [];
   constructor() { }
 
+  // Add to cart
   addToCart(product) {
     this.items.push(product);
   }
 
+  // Get items
   getItems() {
     return this.items;
   }
 
+  // Clear the cart
   clearCart() {
     this.items = [];
     return this.items;
